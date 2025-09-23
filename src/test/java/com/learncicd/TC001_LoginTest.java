d641cd9f136539edbfcbf0fec75f935f5e61c05e
ST 08 <st.qa08@gmail.com>
2025-09-23 18:46:53 +0530

    Add Assertion in TC001_LoginTest.java Update README.md

--- src/test/java/com/learncicd/TC001_LoginTest.java
+++ src/test/java/com/learncicd/TC001_LoginTest.java
@@ -1,6 +1,7 @@
 package com.learncicd;
 
 import base.BaseClass;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TC001_LoginTest extends BaseClass {
@@ -9,5 +10,6 @@ public class TC001_LoginTest extends BaseClass {
     public void loginTest()
     {
         pageFactory.getLoginPage().loginWithCredentials("standard_user", "secret_sauce");
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
     }
 }
